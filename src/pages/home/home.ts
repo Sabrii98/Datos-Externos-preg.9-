91db698cfdf57043874f503f93b60e193dd431c2
Sabrii98 <100363834@alumnos.uc3m.es>
2019-06-15 18:58:17 +0200

    Pregunta 9: datos externos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ProveedorProvider } from '../../providers/proveedor/proveedor';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  usuarios
+
+  constructor(public navCtrl: NavController, public proveedor: ProveedorProvider) {
 
   }
+
+  ionViewDidLoad() {
+    this.proveedor.obtenerDatos()
+    .subscribe(
+
+      (data)=>{this.usuarios=data;},
+      (error)=>{console.log(error);}
+    )
+  }
+
 
 }
